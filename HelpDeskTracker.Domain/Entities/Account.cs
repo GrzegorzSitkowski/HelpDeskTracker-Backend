2876fd6d28dfdc4e2b8f4fb6ff976871d5e9861e
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-09-17 21:58:24 +0200

    Added collection in Account

--- HelpDeskTracker.Domain/Entities/Account.cs
+++ HelpDeskTracker.Domain/Entities/Account.cs
@@ -11,5 +11,6 @@ namespace HelpDeskTracker.Domain.Entities
     {
         public required string Name { get; set; }
         public DateTimeOffset CreateDate { get; set; }
+        public ICollection<AccountUser> AccountUsers { get; set; } = new List<AccountUser>();
     }
 }
