96df43812e6df14eeaa5ebec170a8c75162bb451
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-09-17 21:56:02 +0200

    Added class of Account

---
+++ HelpDeskTracker.Domain/Entities/Account.cs
@@ -0,0 +1,15 @@
+﻿using HelpDeskTracker.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Domain.Entities
+{
+    public class Account : DomainEntity
+    {
+        public required string Name { get; set; }
+        public DateTimeOffset CreateDate { get; set; }
+    }
+}
