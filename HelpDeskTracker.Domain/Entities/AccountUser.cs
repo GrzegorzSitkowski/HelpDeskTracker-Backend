9f664bcd140895f1e3725146827855400c089312
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-09-17 21:57:14 +0200

    Added class of AccountUser

---
+++ HelpDeskTracker.Domain/Entities/AccountUser.cs
@@ -0,0 +1,17 @@
+﻿using HelpDeskTracker.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Domain.Entities
+{
+    public class AccountUser : DomainEntity
+    {
+        public int AccountId { get; set; }
+        public Account Account { get; set; } = default!;
+        public int UserId { get; set; }
+        public User User { get; set; } = default!;
+    }
+}
