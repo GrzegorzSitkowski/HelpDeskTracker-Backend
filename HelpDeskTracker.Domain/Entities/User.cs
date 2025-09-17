14ca72e341dd4fca44440ae7f917db458f6ecbaf
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-09-17 21:55:05 +0200

    Added class of User

---
+++ HelpDeskTracker.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using HelpDeskTracker.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Domain.Entities
+{
+    public class User : DomainEntity
+    {
+        public required string Email { get; set; }
+        public required string HashedPassword { get; set; }
+        public DateTimeOffset RegisterDate { get; set; }
+    }
+}
