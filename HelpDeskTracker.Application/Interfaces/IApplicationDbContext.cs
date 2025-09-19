7d9a25428f198fa6023028ae7949e88a9502e513
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-09-19 22:38:23 +0200

    Creted interface of IApplicationDbContext

---
+++ HelpDeskTracker.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using HelpDeskTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Account> Accounts { get; set; }
+        DbSet<AccountUser> AccountUsers { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
