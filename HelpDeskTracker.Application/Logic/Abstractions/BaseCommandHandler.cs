22a1c4df4f5b6e30bb414dd453d96761a998ff39
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-09-20 18:46:27 +0200

    Added BaseCommandHandler.cs

---
+++ HelpDeskTracker.Application/Logic/Abstractions/BaseCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using HelpDeskTracker.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Application.Logic.Abstractions
+{
+    public class BaseCommandHandler
+    {
+        protected readonly ICurrentAccountProvider _currentAccountProvider;
+        protected readonly IApplicationDbContext _applicationDbContext;
+
+        public BaseCommandHandler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext)
+        {
+            _currentAccountProvider = currentAccountProvider;
+            _applicationDbContext = applicationDbContext;
+        }
+    }
+}
