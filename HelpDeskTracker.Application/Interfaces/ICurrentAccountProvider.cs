1b5e5ef1a83db73bbe6bfe5cc9da73a9c22f4b1e
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-09-20 18:44:35 +0200

    Added ICurrentAccountProvider

---
+++ HelpDeskTracker.Application/Interfaces/ICurrentAccountProvider.cs
@@ -0,0 +1,16 @@
+﻿using HelpDeskTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Application.Interfaces
+{
+    public interface ICurrentAccountProvider
+    {
+        Task<Account> GetAuthenticatedAccount();
+
+        Task<int?> GetAccountId();
+    }
+}
