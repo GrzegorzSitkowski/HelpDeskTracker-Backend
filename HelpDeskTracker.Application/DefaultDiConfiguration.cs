5262a7475dfe92e1214ef7ee9e66dd30cd39b12e
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-09-29 22:47:15 +0200

    Added DefaultDiConfiguration.cs

---
+++ HelpDeskTracker.Application/DefaultDiConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using HelpDeskTracker.Application.Interfaces;
+using HelpDeskTracker.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Application
+{
+    public static class DefaultDiConfiguration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICurrentAccountProvider, CurrentAccountProvider>();
+
+            return services;
+        }
+    }
+}
