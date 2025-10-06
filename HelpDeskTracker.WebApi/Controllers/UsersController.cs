d30fe9839bcc7d3beb1c69771f233db75f94dda3
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-10-06 20:21:40 +0200

    Created UsersController.cs

---
+++ HelpDeskTracker.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using HelpDeskTracker.Application.Logic.User;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDeskTracker.WebApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class UsersController : BaseController
+    {
+        public UsersController(ILogger<UsersController> logger, IMediator mediator) : base(logger, mediator)
+        {
+                
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUserWithAccount([FromBody] CreateUserWithAccountCommand.Request model)
+        {
+            var createAccountResult = await _mediator.Send(model);
+            return Ok(createAccountResult);
+        }
+
+    }
+}
