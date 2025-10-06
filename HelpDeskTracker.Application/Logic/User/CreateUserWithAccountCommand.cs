79c3f13e4ecec8fa95d3b91885e10047dc49a086
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-10-06 20:13:33 +0200

    Added CreateUserWithAccountCommand.cs

---
+++ HelpDeskTracker.Application/Logic/User/CreateUserWithAccountCommand.cs
@@ -0,0 +1,99 @@
+﻿using HelpDeskTracker.Application.Exceptions;
+using HelpDeskTracker.Application.Interfaces;
+using HelpDeskTracker.Application.Logic.Abstractions;
+using HelpDeskTracker.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Application.Logic.User
+{
+    public static class CreateUserWithAccountCommand
+    {
+        public class Request : IRequest<Result>
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public required string Email { get; set; }
+            public required string Password { get; set; }
+            public string Type { get; set; }
+        }
+
+        public class Result
+        {
+            public required int UserId { get; set; }
+        }
+
+        public class Handler : BaseCommandHandler, IRequestHandler<Request, Result>
+        {
+            private readonly IPasswordManager _passwordManager;
+
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext, IPasswordManager passwordManager) : base(currentAccountProvider, applicationDbContext)
+            {
+                _passwordManager = passwordManager;
+            }
+
+            public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var userExists = await _applicationDbContext.Users.AnyAsync(u => u.Email == request.Email);
+
+                if (userExists)
+                {
+                    throw new ErrorException("AccountWithThisEmailAlreadyExists");
+                }
+
+                var utcNow = DateTime.Now;
+                var user = new Domain.Entities.User()
+                {
+                    RegisterDate = utcNow,
+                    Email = request.Email,
+                    HashedPassword = ""
+                };
+
+                user.HashedPassword = _passwordManager.HashPassword(request.Password);
+
+                _applicationDbContext.Users.Add(user);
+
+                var account = new Domain.Entities.Account()
+                {
+                    Name = request.Email,
+                    CreateDate = utcNow,
+                };
+
+                _applicationDbContext.Accounts.Add(account);
+
+                var accountUser = new AccountUser()
+                {
+                    Account = account,
+                    User = user,
+                };
+
+                _applicationDbContext.AccountUsers.Add(accountUser);
+
+                await _applicationDbContext.SaveChangesAsync(cancellationToken);
+
+                return new Result()
+                {
+                    UserId = user.Id,
+                };
+            }
+        }
+
+        public class Validator : AbstractValidator<Request>
+        {
+            public Validator()
+            {
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Email).EmailAddress();
+                RuleFor(x => x.Email).MaximumLength(100);
+
+                RuleFor(x => x.Password).NotEmpty();
+                RuleFor(x => x.Password).MaximumLength(50);
+            }
+        }
+    }
+}
---
+++ HelpDeskTracker.Infrastructure/Auth/AuthConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using HelpDeskTracker.Application.Interfaces;
+using EFCoreSecondLevelCacheInterceptor;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Infrastructure.Auth
+{
+    public static class AuthConfiguration
+    {
+        public static IServiceCollection AddJwtAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<JwtAuthenticationOptions>(configuration.GetSection("JwtAuthentication"));
+            services.AddSingleton<JwtManager>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddPasswordManager(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IPasswordHasher<>), typeof(PasswordHasher<>));
+            services.AddScoped<IPasswordManager, PasswordManager>();
+
+            return services;
+        }
+    }
+}
