bb92b8279b474b69d520e86af3a4beaa4e2456cd
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-10-06 20:15:41 +0200

    Created BsaeController.cs

---
+++ HelpDeskTracker.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDeskTracker.WebApi.Controllers
+{
+    public abstract class BaseController : ControllerBase
+    {
+        protected readonly ILogger _logger;
+        protected readonly IMediator _mediator;
+
+        protected BaseController(ILogger logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+    }
+}
