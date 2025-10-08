29bdf325a9760e024e0d2272a71274acc5105d69
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-10-08 22:35:36 +0200

    Added ValidationBehavior.cs

---
+++ HelpDeskTracker.Application/Validators/ValidationBehavior.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Application.Validators
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IList<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators.ToList();
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (!_validators.Any())
+            {
+                return await next();
+            }
+
+            // Tutaj dodaj implementację walidacji przy użyciu walidatorów i rzucanie wyjątku
+
+            return await next();
+        }
+    }
+}
