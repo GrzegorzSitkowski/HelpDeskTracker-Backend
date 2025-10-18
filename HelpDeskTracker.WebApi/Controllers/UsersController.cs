3030775f32086db7e46d18011cbc4610ed7ecc57
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-10-18 22:51:07 +0200

    Added JwtManger in UsersController.cs

--- HelpDeskTracker.WebApi/Controllers/UsersController.cs
+++ HelpDeskTracker.WebApi/Controllers/UsersController.cs
@@ -1,15 +1,22 @@
 ﻿using HelpDeskTracker.Application.Logic.User;
+using HelpDeskTracker.Infrastructure.Auth;
+using HelpDeskTracker.WebApi.Application.Auth;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace HelpDeskTracker.WebApi.Controllers
 {
     [Route("[controller]/[action]")]
     public class UsersController : BaseController
     {
-        public UsersController(ILogger<UsersController> logger, IMediator mediator) : base(logger, mediator)
+        private readonly CookieSettings? _cookieSettings;
+        private readonly JwtManager _jwtManager;
+
+        public UsersController(ILogger<UsersController> logger, IMediator mediator, IOptions<CookieSettings> cookieSettings, JwtManager jwtManager) : base(logger, mediator)
         {
-                
+            _cookieSettings = cookieSettings != null ? cookieSettings.Value : null;
+            _jwtManager = jwtManager;
         }
 
         [HttpPost]
@@ -17,6 +24,44 @@ namespace HelpDeskTracker.WebApi.Controllers
         {
             var createAccountResult = await _mediator.Send(model);
             return Ok(createAccountResult);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login([FromBody] LoginCommand.Request model)
+        {
+            var loginResult = await _mediator.Send(model);
+            var token = _jwtManager.GenerateUserToken(loginResult.UserId);
+            SetTokenCookie(token);
+            return Ok(new JwtToken() { AccessToken = token });
+        }
+
+        public class JwtToken
+        {
+            public string? AccessToken { get; set; }
+        }
+
+        private void SetTokenCookie(string token)
+        {
+            var cookieOption = new CookieOptions()
+            {
+                HttpOnly = true,
+                Secure = true,
+                Expires = DateTime.Now.AddDays(30),
+                SameSite = SameSiteMode.Lax,
+            };
+
+            if (_cookieSettings != null)
+            {
+                cookieOption = new CookieOptions()
+                {
+                    HttpOnly = cookieOption.HttpOnly,
+                    Expires = cookieOption.Expires,
+                    Secure = _cookieSettings.Secure,
+                    SameSite = _cookieSettings.SameSite,
+                };
+            }
+
+            Response.Cookies.Append(CookieSettings.CookieName, token, cookieOption);
         }
 
     }
