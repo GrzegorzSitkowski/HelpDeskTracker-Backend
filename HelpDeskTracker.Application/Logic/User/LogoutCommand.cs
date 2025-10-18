c05a1cbe2caf5c748020308ebebab0b15e53cb93
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-10-18 22:52:46 +0200

    Created LogoutCommand.cs

---
+++ HelpDeskTracker.Application/Logic/User/LogoutCommand.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using HelpDeskTracker.Application.Interfaces;
+using HelpDeskTracker.Application.Logic.Abstractions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Application.Logic.User
+{
+    public static class LogoutCommand
+    {
+        public class Request : IRequest<Result>
+        {
+        }
+
+        public class Result
+        {
+        }
+
+        public class Handler : BaseCommandHandler, IRequestHandler<Request, Result>
+        {
+            private readonly IPasswordManager _passwordManager;
+
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext) : base(currentAccountProvider, applicationDbContext)
+            {
+            }
+
+            public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+            {
+                return new Result()
+                {
+
+                };
+            }
+        }
+
+        public class Validator : AbstractValidator<Request>
+        {
+            public Validator()
+            {
+            }
+        }
+    }
+}
