91187737f0f85c57b45d252823a687cf954f0355
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-10-19 20:35:19 +0200

    Added AccountController.cs

---
+++ HelpDeskTracker.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using HelpDeskTracker.Application.Logic.Account;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDeskTracker.WebApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class AccountsController : BaseController
+    {
+        public AccountsController(ILogger<AccountsController> logger,
+            IMediator mediator) : base(logger, mediator)
+        {
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetCurrentAccount()
+        {
+            var data = await _mediator.Send(new CurrentAccountQuery.Request() { });
+            return Ok(data);
+        }
+
+    }
+}
